dbd7bfbea0253756aa91dc0fef2e9febc2eb9a8e
Andy DeSoto <kadesoto@me.com>
2014-04-03 16:49:32 -0500

    Fixed things

--- python.py
+++ python.py
@@ -1,10 +1,8 @@
-import serial
 import smtplib
 import threading
 import time
 import datetime
 import subprocess
-from apscheduler.scheduler import Scheduler
 from email.mime.text import MIMEText
 
 class LabMember:
@@ -23,8 +21,6 @@ class LabMember:
 USERNAME = "skulving@technimentis.com"
 PASSWORD = "tulvingtulving"
 
-ser = serial.Serial('/dev/ttyACM0', 2400)
-
 andy = LabMember("Andy", "Andy", "DeSoto", "DeSoto", "Mister", "5A", "@kadesoto")
 adam = LabMember("Adam", "Adam", "Putnam", "Putnam", "Mister", "D8", "@adamlputnam")
 jason = LabMember("Jason", "Jason", "Finley", "Finley", "Doctor", "5C", "@jasonrfinley")
@@ -38,22 +34,8 @@ roddy = LabMember("Roddy", "Roddy", "Roediger", "Roediger", "Doctor", "XX", "")
 
 lab = [andy, adam, jason, john, victor, julie, pooja, meghan, allison, roddy]
 
-def main():    
-    sched = Scheduler()
-    sched.start()
-    
-    sched.add_cron_job(BBCAlert, day_of_week='wed', hour = '15', minute = '55') # BBC Alert should go off at 3:55 PM Wednesdays
-    sched.add_cron_job(labMeetingAlert, day_of_week='mon', hour = '10', minute = '25') # lab meeting alert should go off at 10:25 AM Mondays
-    sched.add_cron_job(checkoutEveryone, hour = '23', minute = '55') # reset daily variables at 11:55 PM every day
-    
-    while 1 :
-        #print("Listening for serial input...") # for debug
-        serialInput = ser.readline()
-        serialInput = serialInput.strip()
-        if len(serialInput) == 10:
-            #print ("Found serial input! Trimming it...") # for debug
-            swipe(serialInput[-2:]) # get the last 2 characters of the serial input (i.e., RFID tag)
-                                         
+def main():
+    print("In main.")
 
 def swipe(swipeCode):
     print("Swiping... " + swipeCode)
@@ -65,7 +47,7 @@ def swipe(swipeCode):
          else:
             temporaryTwitterHandle = ""
          sendIFTTTEmail("Tweet #tweet", x.firstName + temporaryTwitterHandle + "has checked into the Memory Lab.")
-         speak("Welcome to the Memory Lab, " + x.title + " " + x.firstNamePhonetic + " " + x.lastNamePhonetic)
+         print("Welcome to the Memory Lab, " + x.title + " " + x.firstNamePhonetic + " " + x.lastNamePhonetic)
          x.timeIn = datetime.datetime.now()
          x.present = True
          
@@ -75,11 +57,10 @@ def swipe(swipeCode):
          else:
             temporaryTwitterHandle = ""
          sendIFTTTEmail("Tweet #tweet", x.firstName + temporaryTwitterHandle + "has checked out of the Memory Lab.")
-         speak("Farewell, " + x.title + " " + x.lastNamePhonetic)
+         print("Farewell, " + x.title + " " + x.lastNamePhonetic)
          x.timeOut = datetime.datetime.now()
          x.present = False
-         time.sleep(5)
-         sendIFTTTEmail(x.firstName + " " + x.LastName + " #loghours", str(x.timeOut - x.timeIn))
+         sendIFTTTEmail(x.firstName + " " + x.lastName + " #loghours", str(x.timeOut - x.timeIn))
 
 def checkoutEveryone():
     for x in lab:
@@ -116,17 +97,17 @@ def reportIP():
     server.sendmail(USERNAME, "kadesoto@gmail.com", msg.as_string())
     server.quit()
     print("Email hopefully sent...")
-    speak("Ready.")
+    print("Ready.")
     s.close()
-
-def speak(speechString):
-    subprocess.call(["./speech.sh", speechString])
     
 def BBCAlert():
-    speak("The BBC colloquium is about to start.")
+    print("The BBC colloquium is about to start.")
     
 def labMeetingAlert():
-    speak("The Roediger lab meeting is about to start.")    
+    print("The Roediger lab meeting is about to start.")    
 
 reportIP()
 main()
+swipe("5A")
+time.sleep(10)
+swipe("5A")
